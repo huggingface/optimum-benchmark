325b52ec74ef6bc97671f6b7f7cbf9e28a4e3735
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2023-06-03 15:36:52 +0200

    updated reporter

--- reporter.py
+++ reporter.py
@@ -14,26 +14,25 @@ def gather_inference_report(
     stats_files = [
         stats_file for stats_file in folder.glob(f"**/inference_results.csv")
     ]
+    stats_dfs = {i: pd.read_csv(f, index_col=0) for i, f in enumerate(stats_files)}
+
     configs_files = [
         config_file for config_file in folder.glob(f"**/hydra_config.yaml")
     ]
-
-    # only leave config files that have the same parent as stats files
+    # only leave config files that have a corresponding stats file
     configs_files = [
         config_file
         for config_file in configs_files
         if config_file.parent in [stats_file.parent for stats_file in stats_files]
     ]
-
-    stats_dfs = {i: pd.read_csv(f, index_col=0) for i, f in enumerate(stats_files)}
     config_dicts = {
         i: flatten(OmegaConf.load(f), reducer="dot")
         for i, f in enumerate(configs_files)
     }
-
     # for now there's a problem with list of operators to quantize
     for d in config_dicts.values():
-        d.pop("backend.quantization.operators_to_quantize", None)
+        d.pop("backend.quantization_config.operators_to_quantize", None)
+    
     configs_dfs = {i: pd.DataFrame(d, index=[0]) for i, d in config_dicts.items()}
 
     if len(stats_dfs) == 0 or len(configs_dfs) == 0:
@@ -48,11 +47,12 @@ def gather_inference_report(
     }
     # Concatenate all reports
     inference_report = pd.concat(inference_reports.values(), axis=0, ignore_index=True)
-    inference_report["config_path"] = configs_files
-    inference_report.set_index("config_path", inplace=True)
+    inference_report["Config Path"] = configs_files  # for console display (clickable)
+    inference_report.set_index("Config Path", inplace=True)
     # sort by throughput and remove failed experiments
-    inference_report = inference_report[inference_report["throughput(it/s)"] > 0.0]
-    inference_report.sort_values(by=["throughput(it/s)"], ascending=False, inplace=True)
+    inference_report.sort_values(
+        by=["Model.Throughput(iter/s)"], ascending=False, inplace=True
+    )
 
     return inference_report
 
@@ -60,9 +60,14 @@ def gather_inference_report(
 def show_inference_report(report, with_baseline=False):
     # columns to display
     show_report = report[
-        (["baseline"] if with_baseline else [])
-        + ["latency.mean(s)", "latency.median(s)", "throughput(it/s)"]
-        + (["speedup(%)"] if with_baseline else [])
+        (["Baseline"] if with_baseline else [])
+        + ["Model.Latency(s)", "Model.Throughput(iter/s)"]
+        + (["Speedup(%)"] if with_baseline else [])
+        + (
+            ["Generation.Throughput(tok/s)"]
+            if "Generation.Throughput(tok/s)" in report.columns
+            else []
+        )
     ]
 
     table = Table(
@@ -74,7 +79,7 @@ def show_inference_report(report, with_baseline=False):
         [tuple(col.split(".")) for col in show_report.columns.to_list()]
     )
 
-    table.add_column("config_path", justify="left")
+    table.add_column("Config Path", justify="left")
     for level in range(show_report.columns.nlevels):
         columns = show_report.columns.get_level_values(level).to_list()
         for i in range(len(columns)):
@@ -116,19 +121,19 @@ def show_inference_report(report, with_baseline=False):
 
 
 def main(args):
-    experiments_report = gather_inference_report(args.experiments_folder)
+    experiments_report = gather_inference_report(args.sweeps_folder)
     if args.baseline_folder is not None:
         print("Using the provided baseline")
         baseline_report = gather_inference_report(args.baseline_folder)
         assert len(baseline_report) == 1, "There should be only one baseline"
-        experiments_report["baseline"] = [False] * len(experiments_report)
-        experiments_report["speedup(%)"] = (
-            experiments_report["throughput(it/s)"]
-            / baseline_report["throughput(it/s)"].iloc[0]
+        experiments_report["Baseline"] = [False] * len(experiments_report)
+        experiments_report["Speedup(%)"] = (
+            experiments_report["Model.Throughput(iter/s)"]
+            / baseline_report["Model.Throughput(iter/s)"].iloc[0]
             - 1
         ) * 100
-        baseline_report["baseline"] = True
-        baseline_report["speedup(%)"] = 0
+        baseline_report["Baseline"] = [True] * len(baseline_report)
+        baseline_report["Speedup(%)"] = [0] * len(baseline_report)
 
         report = pd.concat(
             [experiments_report, baseline_report], axis=0, ignore_index=False
@@ -137,18 +142,18 @@ def main(args):
         print("No baseline provided")
         report = experiments_report
 
-    report.to_csv(f"{args.experiments_folder}/inference_report.csv", index=True)
+    report.to_csv(f"{args.sweeps_folder}/inference_report.csv", index=True)
     show_inference_report(report, with_baseline=args.baseline_folder is not None)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
-        "--experiments-folder",
-        "-e",
+        "--sweeps-folder",
+        "-s",
         type=Path,
         default="sweeps/",
-        help="The folder containing the results of the benchmark.",
+        help="The folder containing the results of sweeps.",
     )
     parser.add_argument(
         "--baseline-folder",
