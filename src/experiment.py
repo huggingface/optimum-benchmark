b2fcac9f7cecc73561d74053e0e035f532bee557
ilyas@huggingface.co <moutawwakil.ilyas.tsi@gmail.com>
2023-07-17 23:34:27 +0000

    task can be specified when onnx files are local

--- src/experiment.py
+++ src/experiment.py
@@ -1,15 +1,25 @@
 from dataclasses import dataclass, MISSING
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from logging import getLogger
 import platform
 import os
 
+from optimum.exporters import TasksManager
 from optimum.version import __version__ as optimum_version
-from transformers import __version__ as transformers_version  # type: ignore
+from transformers import __version__ as transformers_version
 
 from src.backend.base import BackendConfig
 from src.benchmark.inference import BenchmarkConfig
 from src.utils import get_cpu, get_cpu_ram_mb
+
+OmegaConf.register_new_resolver(
+    "infer_task",
+    lambda model, subfolder, revision : TasksManager.infer_task_from_model(
+            model=model,
+            subfolder=subfolder,
+            revision=revision,
+        ),
+)
 
 LOGGER = getLogger("experiment")
 
@@ -28,6 +38,8 @@ class ExperimentConfig:
     model: str = MISSING  # type: ignore
     # Device name or path (cpu, cuda, cuda:0, ...)
     device: str = MISSING  # type: ignore
+    # task
+    task: str = "${infer_task:${model}, ${cache_kwargs.subfolder}, ${cache_kwargs.revision}}"
 
     # ADDITIONAL MODEL CONFIGURATION: Model revision, use_auth_token, trust_remote_code
     cache_kwargs: DictConfig = DictConfig(
