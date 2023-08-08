a0a6718be51e55ed853bebe5f12daebd86c576a5
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2023-08-08 17:24:36 +0200

    updated tests and added gpu workflows

---
+++ examples/docker/local_gpu_onnxruntime_training.sh
@@ -0,0 +1,7 @@
+# Run the tests on a local GPU
+
+echo "Building image"
+docker build -f examples/docker/gpu_onnxruntime_training.dockerfile -t gpu-onnxruntime-training .
+
+echo "Running tests within the container"
+docker run --rm --gpus all --workdir=/workspace/optimum-benchmark gpu-onnxruntime-training
--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     name="optimum-benchmark",
     version="0.0.1",
     packages=find_packages(),
+    # add pytest as for optimum-benchmark[test]
+    extras_require={
+        "test": ["pytest"],
+    },
     entry_points={
         "console_scripts": [
             "optimum-benchmark=optimum_benchmark.main:run_experiment",
