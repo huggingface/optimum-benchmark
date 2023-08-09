fe3de0b22f5d71692c06ee77d6f2a9261f137810
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2023-08-09 13:15:08 +0200

    updated tests to run on a self-hosted runner

--- examples/docker/local_gpu.sh
+++ examples/docker/local_gpu.sh
@@ -1,7 +1,7 @@
 # Run the tests on a local GPU
 
 echo "Building image"
-docker build -f examples/docker/gpu.dockerfile -t optimum-benchmark-gpu .
+docker build -f examples/docker/gpu.Dockerfile -t optimum-benchmark-gpu .
 
 echo "Running tests within the container"
 docker run --rm --gpus all --workdir=/workspace/optimum-benchmark optimum-benchmark-gpu
