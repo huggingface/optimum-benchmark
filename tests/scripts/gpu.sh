e42692425685f4ddf0677f4029d0c2949b51a313
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2023-08-14 15:26:14 +0200

    fix script

--- tests/scripts/gpu.sh
+++ tests/scripts/gpu.sh
@@ -1,4 +1,4 @@
-docker run --rm --gpus all -v $(pwd):/workspace/optimum-benchmark --workdir=/workspace/optimum-benchmark optimum-benchmark-gpu \bin\bash -c "\
+docker run --rm --gpus all -v $(pwd):/workspace/optimum-benchmark --workdir=/workspace/optimum-benchmark optimum-benchmark-gpu /bin/bash -c "\
 pip install -r gpu_requirements.txt \
 && pip install -e .[test] \
 && pytest -k '(cuda or tensorrt) and not onnxruntime_training'"
