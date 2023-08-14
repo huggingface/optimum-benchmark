035ca8d5e87c16816839892df67afdf6e66d901d
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2023-08-14 16:19:11 +0200

    added cleanup

--- tests/scripts/gpu_onnxruntime_training.sh
+++ tests/scripts/gpu_onnxruntime_training.sh
@@ -1,4 +1,5 @@
 docker run --rm --gpus all -v $(pwd):/workspace/optimum-benchmark --workdir=/workspace/optimum-benchmark gpu-onnxruntime-training /bin/bash -c "\
 pip install -r gpu_onnxruntime_training_requirements.txt \
 && pip install -e .[test] \
-&& pytest -k '(cuda or tensorrt) and onnxruntime_training'"
+&& pytest -k '(cuda or tensorrt) and onnxruntime_training' \
+&& rm -rf /workspace/optimum-benchmark/*"
