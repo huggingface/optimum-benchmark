af5d15a7a7027b10587e6bebee39517c4270732c
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2023-08-31 04:03:46 +0200

    add fallback to offline emission tracker

--- optimum_benchmark/trackers/energy.py
+++ optimum_benchmark/trackers/energy.py
@@ -2,7 +2,7 @@ import os
 from contextlib import contextmanager
 from logging import getLogger
 
-from codecarbon import EmissionsTracker
+from codecarbon import EmissionsTracker, OfflineEmissionsTracker
 
 LOGGER = getLogger("latency_tracker")
 
@@ -14,11 +14,31 @@ class EnergyTracker:
 
     @contextmanager
     def track(self, interval=1, file_prefix=""):
-        self.emission_tracker = EmissionsTracker(
-            measure_power_secs=interval,
-            output_file=f"{file_prefix}_codecarbon.csv",
-            gpu_ids=os.environ.get("CUDA_VISIBLE_DEVICES", None),
-        )
+        try:
+            self.emission_tracker = EmissionsTracker(
+                log_level="error",  # "info" for more verbosity
+                tracking_mode="process",  # "machine" for machine-level tracking
+                measure_power_secs=interval,
+                output_file=f"{file_prefix}_codecarbon.csv",
+                gpu_ids=os.environ.get("CUDA_VISIBLE_DEVICES", None),
+            )
+        except Exception as e:
+            LOGGER.warning(f"Failed to initialize Online Emissions Tracker: {e}")
+            LOGGER.info("Falling back to Offline Emissions Tracker")
+            country_iso_code = os.environ.get("COUNTRY_ISO_CODE", None)
+            if country_iso_code is None:
+                raise ValueError(
+                    "COUNTRY_ISO_CODE environment variable must be set when using Offline Emissions Tracker"
+                )
+            self.emission_tracker = OfflineEmissionsTracker(
+                log_level="error",
+                tracking_mode="process",
+                measure_power_secs=interval,
+                country_iso_code=country_iso_code,
+                output_file=f"{file_prefix}_codecarbon.csv",
+                gpu_ids=os.environ.get("CUDA_VISIBLE_DEVICES", None),
+            )
+
         self.emission_tracker.start()
         yield
         self.emission_tracker.stop()
