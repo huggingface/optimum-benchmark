84a12a1017d82089dcde9c93d1393c30ad8ea7bc
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2023-10-17 12:18:40 +0200

    update optimum

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ OPTIMUM_VERSION = "1.13.0"
 
 INSTALL_REQUIRES = [
     # Mandatory HF dependencies
-    f"optimum=={OPTIMUM_VERSION}",  # backends, tasks and input generation
+    f"optimum>={OPTIMUM_VERSION}",  # backends, tasks and input generation
     "accelerate",  # distributed inference and no weights init
     # Hydra
     "omegaconf==2.3.0",
@@ -33,10 +33,10 @@ EXTRAS_REQUIRE = {
     "quality": ["black", "ruff"],
     "report": ["flatten_dict", "matplotlib", "seaborn", "rich"],
     # cpu backends
-    "openvino": [f"optimum[openvino,nncf]=={OPTIMUM_VERSION}"],
-    "onnxruntime": [f"optimum[onnxruntime]=={OPTIMUM_VERSION}"],
-    "onnxruntime-gpu": [f"optimum[onnxruntime-gpu]=={OPTIMUM_VERSION}"],
-    "neural-compressor": [f"optimum[neural-compressor]=={OPTIMUM_VERSION}"],
+    "openvino": [f"optimum[openvino,nncf]>={OPTIMUM_VERSION}"],
+    "onnxruntime": [f"optimum[onnxruntime]>={OPTIMUM_VERSION}"],
+    "onnxruntime-gpu": [f"optimum[onnxruntime-gpu]>={OPTIMUM_VERSION}"],
+    "neural-compressor": [f"optimum[neural-compressor]>={OPTIMUM_VERSION}"],
     # server-like backend
     "text-generation-inference": ["docker==6.1.3"],
     # specific settings
