efa1623aa5220544d7671beae9ae317b80bfc316
Ilyas Moutawwakil <57442720+IlyasMoutawwakil@users.noreply.github.com>
2024-03-21 09:49:35 +0100

    Update zentorch plugin (#167)

--- optimum_benchmark/backends/pytorch/backend.py
+++ optimum_benchmark/backends/pytorch/backend.py
@@ -18,7 +18,7 @@ from transformers import (
     TrainingArguments,
 )
 
-from ...import_utils import is_deepspeed_available, is_torch_distributed_available, is_torch_zendnn_plugin_available
+from ...import_utils import is_deepspeed_available, is_torch_distributed_available, is_zentorch_available
 from ..base import Backend
 from ..peft_utils import apply_peft
 from ..transformers_utils import random_init_weights
@@ -30,8 +30,8 @@ if is_deepspeed_available():
 if is_torch_distributed_available():
     import torch.distributed
 
-if is_torch_zendnn_plugin_available():
-    import torch_zendnn_plugin  # noqa: F401, F403
+if is_zentorch_available():
+    import zentorch  # type: ignore # noqa: F401
 
 
 # bachend logger
