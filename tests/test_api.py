66a3f1a298f4b3764e77b1c03ac84857fc8dc4e7
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-03-26 22:18:14 +0100

    fixes

--- tests/test_api.py
+++ tests/test_api.py
@@ -28,91 +28,25 @@ from optimum_benchmark.trackers.latency import LatencyTracker
 from optimum_benchmark.trackers.memory import MemoryTracker
 
 LIBRARIES_TASKS_MODELS = [
-    ("transformers", "fill-mask", "bert-base-uncased"),
+    ("timm", "image-classification", "timm/resnet50.a1_in1k"),
     ("transformers", "text-generation", "openai-community/gpt2"),
+    ("transformers", "fill-mask", "google-bert/bert-base-uncased"),
     ("transformers", "text2text-generation", "google-t5/t5-small"),
     ("transformers", "multiple-choice", "FacebookAI/roberta-base"),
-    ("transformers", "feature-extraction", "distilbert-base-uncased"),
     ("transformers", "text-classification", "FacebookAI/roberta-base"),
     ("transformers", "token-classification", "microsoft/deberta-v3-base"),
     ("transformers", "image-classification", "google/vit-base-patch16-224"),
     ("transformers", "semantic-segmentation", "google/vit-base-patch16-224"),
     ("diffusers", "stable-diffusion", "CompVis/stable-diffusion-v1-4"),
-    ("timm", "image-classification", "timm/resnet50.a1_in1k"),
 ]
-BENCHMARKS = ["training", "inference"]
-LAUNCHERS = ["inline", "process"]
-BACKENDS = ["pytorch", "other"]
-DEVICES = ["cpu", "cuda"]
 
 
-@pytest.mark.parametrize("device", DEVICES)
-@pytest.mark.parametrize("backend", BACKENDS)
-def test_api_latency_tracker(device, backend):
-    expected_latency = 1
-    tracker = LatencyTracker(device=device, backend=backend)
-
-    for _ in range(2):
-        with tracker.track():
-            time.sleep(1)
-
-    latency = tracker.get_latency()
-    latency.log()
-
-    assert latency.mean < expected_latency * 1.1
-    assert latency.mean > expected_latency * 0.9
-
-
-@pytest.mark.parametrize("device", DEVICES)
-@pytest.mark.parametrize("backend", BACKENDS)
-def test_api_memory_tracker(device, backend):
-    tracker = MemoryTracker(device=device, backend=backend)
-
-    tracker.reset()
-    with tracker.track():
-        time.sleep(1)
-        pass
-
-    # the process consumes memory that we can't control
-    initial_memory = tracker.get_max_memory()
-    initial_memory.log()
-
-    tracker.reset()
-    with tracker.track():
-        time.sleep(1)
-        array = torch.randn((10000, 10000), dtype=torch.float64, device=device)
-        expected_memory = array.nbytes / 1e6
-        time.sleep(1)
-
-    final_memory = tracker.get_max_memory()
-    final_memory.log()
-
-    if device == "cuda":
-        if backend == "pytorch":
-            measured_memory = final_memory.max_allocated - initial_memory.max_allocated
-        else:
-            measured_memory = final_memory.max_vram - initial_memory.max_vram
-            if torch.version.hip is not None:
-                return  # skip vram measurement for ROCm
-    else:
-        measured_memory = final_memory.max_ram - initial_memory.max_ram
-
-    assert measured_memory < expected_memory * 1.1
-    assert measured_memory > expected_memory * 0.9
-
-    del array
-
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-
-    gc.collect()
-
-
-@pytest.mark.parametrize("device", DEVICES)
-@pytest.mark.parametrize("launcher", LAUNCHERS)
-@pytest.mark.parametrize("benchmark", BENCHMARKS)
+@pytest.mark.parametrize("device", ["cpu", "cuda"])
+@pytest.mark.parametrize("launcher", ["inline", "process"])
+@pytest.mark.parametrize("benchmark", ["training", "inference"])
 @pytest.mark.parametrize("library,task,model", LIBRARIES_TASKS_MODELS)
 def test_api_launch(device, launcher, benchmark, library, task, model):
+    no_weights = True if library == "transformers" else False
     device_ids = "0" if device == "cuda" else None
 
     if benchmark == "training":
@@ -126,8 +60,8 @@ def test_api_launch(device, launcher, benchmark, library, task, model):
         launcher_config = InlineConfig(device_isolation=False)
 
     backend_config = PyTorchConfig(
+        no_weights=no_weights,
         device_ids=device_ids,
-        no_weights=True,
         device=device,
         task=task,
         model=model,
@@ -195,6 +129,69 @@ def test_api_dataset_generator(library, task, model):
     assert len(generated_dataset) > 0, "No dataset was generated"
 
     len(generated_dataset) == DATASET_SHAPES["dataset_size"]
+
+
+@pytest.mark.parametrize("device", ["cpu", "cuda"])
+@pytest.mark.parametrize("backend", ["pytorch", "other"])
+def test_api_latency_tracker(device, backend):
+    expected_latency = 1
+    tracker = LatencyTracker(device=device, backend=backend)
+
+    for _ in range(2):
+        with tracker.track():
+            time.sleep(1)
+
+    latency = tracker.get_latency()
+    latency.log()
+
+    assert latency.mean < expected_latency * 1.1
+    assert latency.mean > expected_latency * 0.9
+
+
+@pytest.mark.parametrize("device", ["cpu", "cuda"])
+@pytest.mark.parametrize("backend", ["pytorch", "other"])
+def test_api_memory_tracker(device, backend):
+    tracker = MemoryTracker(device=device, backend=backend)
+
+    tracker.reset()
+    with tracker.track():
+        time.sleep(1)
+        pass
+
+    # the process consumes memory that we can't control
+    initial_memory = tracker.get_max_memory()
+    initial_memory.log()
+
+    tracker.reset()
+    with tracker.track():
+        time.sleep(1)
+        array = torch.randn((10000, 10000), dtype=torch.float64, device=device)
+        expected_memory = array.nbytes / 1e6
+        time.sleep(1)
+
+    final_memory = tracker.get_max_memory()
+    final_memory.log()
+
+    if device == "cuda":
+        if backend == "pytorch":
+            measured_memory = final_memory.max_allocated - initial_memory.max_allocated
+        else:
+            if torch.version.hip is not None:
+                return  # skip vram measurement for ROCm
+
+            measured_memory = final_memory.max_vram - initial_memory.max_vram
+    else:
+        measured_memory = final_memory.max_ram - initial_memory.max_ram
+
+    assert measured_memory < expected_memory * 1.1
+    assert measured_memory > expected_memory * 0.9
+
+    del array
+
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+
+    gc.collect()
 
 
 def test_git_revision_hash_detection():
