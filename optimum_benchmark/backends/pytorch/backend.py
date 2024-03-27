36d41ad58fd5c55b6973281a06d9f55e13b9dde5
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-03-27 12:17:37 +0100

    try to fix

--- optimum_benchmark/backends/pytorch/backend.py
+++ optimum_benchmark/backends/pytorch/backend.py
@@ -101,6 +101,7 @@ class PyTorchBackend(Backend[PyTorchConfig]):
         if self.config.torch_compile:
             if self.config.library == "diffusers":
                 LOGGER.info("\t+ Using torch.compile to compile unet and vae")
+                torch._inductor.config.conv_1x1_as_mm = True
                 self.pretrained_model.unet = torch.compile(
                     self.pretrained_model.unet, **self.config.torch_compile_config
                 )
