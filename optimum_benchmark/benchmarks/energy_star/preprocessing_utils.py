5c70185f6e0994cbe798b43e178b9fc70e50cebe
Sasha <sasha.luccioni@huggingface.co>
2024-04-12 09:31:05 -0400

    trying image_classification

--- optimum_benchmark/benchmarks/energy_star/preprocessing_utils.py
+++ optimum_benchmark/benchmarks/energy_star/preprocessing_utils.py
@@ -11,7 +11,8 @@ def preprocess(dataset: Dataset, task: str, config: EnergyStarConfig, preprocess
         "text-classification": text_classification_preprocessing,
         "question-answering": question_answering_preprocessing,
         "text-generation": text_generation_preprocessing,
-        "image-to-text": image_preprocessing,
+        "image-to-text": image_text_preprocessing,
+        "image-classification": image_preprocessing,
         'automatic-speech-recognition': audio_preprocessing
     }
 
@@ -164,8 +165,31 @@ def text_generation_preprocessing(
 
     return dataset
 
-
 def image_preprocessing(dataset: Dataset, config: EnergyStarConfig, processor: PretrainedProcessor) -> Dataset:
+    # Remove empty samples when batch_size is 1 because empty inputs will make the model fail
+    if config.input_shapes["batch_size"] == 1:
+        dataset = dataset.filter(lambda example: example[config.image_column_name] != "")
+
+    if config.num_samples != -1:
+        dataset = dataset.select(range(config.num_samples))
+        # Add a pad token if the tokenizer doesn't have one
+
+    def preprocess_function(examples):
+        return processor(examples[config.image_column_name].convert('RGB'))
+
+    dataset = dataset.map(
+        preprocess_function,
+        batched=True,
+        remove_columns=dataset.features,
+        desc="Running processor on dataset",
+    ).with_format(
+        "torch"
+    )  # We don't want a torch dependency here but for now the only backend used for this benchmark is PyTorch
+
+    return dataset
+
+
+def image_text_preprocessing(dataset: Dataset, config: EnergyStarConfig, processor: PretrainedProcessor) -> Dataset:
     # Remove empty samples when batch_size is 1 because empty inputs will make the model fail
     if config.input_shapes["batch_size"] == 1:
         dataset = dataset.filter(lambda example: example[config.image_column_name] != "")
