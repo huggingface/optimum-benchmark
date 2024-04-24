85eb903854c53f04c5bce4a4a3efbc526e3f9ed6
Sasha <sasha.luccioni@huggingface.co>
2024-04-24 09:46:02 -0400

    wth

--- optimum_benchmark/benchmarks/energy_star/preprocessing_utils.py
+++ optimum_benchmark/benchmarks/energy_star/preprocessing_utils.py
@@ -83,9 +83,8 @@ def summarization_preprocessing(
             examples[config.text_column_name],
             padding=padding,
             truncation=config.truncation,
-            max_length=tokenizer.model_max_length if tokenizer.model_max_length != None else 500 if tokenizer.model_max_length > 10000000000000000 else 500,
+            max_length=tokenizer.model_max_length if tokenizer.model_max_length != None else 100 if tokenizer.model_max_length > 10000000000000000 else 500,
         )
-    print(tokenizer.model_max_length)
     dataset = dataset.map(
         tokenize_function,
         batched=True,
