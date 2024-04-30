be47dc47d14563eac829aac691329f0e230cc538
Sasha <sasha.luccioni@huggingface.co>
2024-04-30 13:46:21 -0400

    smdh

--- optimum_benchmark/benchmarks/energy_star/preprocessing_utils.py
+++ optimum_benchmark/benchmarks/energy_star/preprocessing_utils.py
@@ -207,7 +207,8 @@ def text2text_generation_preprocessing(
             truncation=config.truncation,
             return_token_type_ids=False,
             padding=padding,
-            max_length = getattr(pretrained_config, "max_position_embeddings", 512)- len(tokenizer(config.dataset_prefix))
+            #max_length = getattr(pretrained_config, "max_position_embeddings", 512)- len(tokenizer(config.dataset_prefix))
+            max_length = 128,
             )
 
     dataset=dataset.map(add_prefix)
