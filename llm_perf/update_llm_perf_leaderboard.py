241dc661eadf3a79527aa74dafa2b0d74a27b2cb
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-05-11 18:10:48 +0200

    one file

--- llm_perf/update_llm_perf_leaderboard.py
+++ llm_perf/update_llm_perf_leaderboard.py
@@ -1,4 +1,3 @@
-import os
 from glob import glob
 from tempfile import TemporaryDirectory
 
@@ -8,40 +7,31 @@ from tqdm import tqdm
 
 from optimum_benchmark import Benchmark
 
-SUBSET = os.getenv("SUBSET")
-MACHINE = os.getenv("MACHINE")
 
-PULL_REPO_ID = f"optimum-benchmark/llm-perf-pytorch-cuda-{SUBSET}-{MACHINE}"
+def gather_benchmarks(subset: str, machine: str):
+    pull_repo_id = f"optimum-benchmark/llm-perf-pytorch-cuda-{subset}-{machine}"
 
-print("Pulling benchmark data from", PULL_REPO_ID)
-snapshot = snapshot_download(
-    repo_id=PULL_REPO_ID,
-    repo_type="dataset",
-    allow_patterns=["**/benchmark.json"],
-)
+    snapshot = snapshot_download(repo_type="dataset", repo_id=pull_repo_id, allow_patterns=["**/benchmark.json"])
 
-
-def gather_benchmarks():
     dfs = []
+
     for file in tqdm(glob(f"{snapshot}/**/benchmark.json", recursive=True)):
         dfs.append(Benchmark.from_json(file).to_dataframe())
 
     benchmarks = pd.concat(dfs, ignore_index=True)
-    return benchmarks
 
+    tmp_dir = TemporaryDirectory()
+    push_repo_id = "optimum-benchmark/llm-perf-leaderboard"
+    file_name = f"llm-perf-leaderboard-{subset}-{machine}.csv"
 
-benchmarks = gather_benchmarks()
+    benchmarks.to_csv(f"{tmp_dir}/{file_name}", index=False)
 
-with TemporaryDirectory() as tmp_dir:
-    PUSH_REPO_ID = "optimum-benchmark/llm-perf-leaderboard"
-    FILE_NAME = f"llm-perf-leaderboard-{SUBSET}-{MACHINE}.csv"
-
-    benchmarks.to_csv(f"{tmp_dir}/{FILE_NAME}", index=False)
-
-    create_repo(repo_id=PUSH_REPO_ID, repo_type="dataset", private=True, exist_ok=True)
+    create_repo(repo_id=push_repo_id, repo_type="dataset", private=True, exist_ok=True)
     upload_file(
-        path_or_fileobj=f"{tmp_dir}/{FILE_NAME}",
-        path_in_repo=FILE_NAME,
-        repo_id=PUSH_REPO_ID,
-        repo_type="dataset",
+        path_or_fileobj=f"{tmp_dir}/{file_name}", path_in_repo=file_name, repo_id=push_repo_id, repo_type="dataset"
     )
+
+
+for subset in ["unquantized", "bnb", "awq", "gptq"]:
+    for machine in ["1xA10"]:
+        gather_benchmarks(subset, machine)
