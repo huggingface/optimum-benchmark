b8140858ee0d5f1144d8fde87af1a56879dc3661
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-05-17 09:50:01 +0200

    drop legacy stable-diffusion pipeline

--- optimum_benchmark/task_utils.py
+++ optimum_benchmark/task_utils.py
@@ -41,8 +41,6 @@ _DIFFUSERS_TASKS_TO_MODEL_LOADERS = {
     "inpainting": "AutoPipelineForInpainting",
     "text-to-image": "AutoPipelineForText2Image",
     "image-to-image": "AutoPipelineForImage2Image",
-    "stable-diffusion": "StableDiffusionPipeline",  # legacy
-    "stable-diffusion-xl": "StableDiffusionXLImg2ImgPipeline",  # legacy
 }
 _TIMM_TASKS_TO_MODEL_LOADERS = {
     "image-classification": "create_model",
@@ -146,9 +144,10 @@ def infer_task_from_model_name_or_path(model_name_or_path: str, revision: Option
             inferred_task_name = "text-to-image"
         elif "image-to-image" in model_info.tags:
             inferred_task_name = "image-to-image"
+        elif "inpainting" in model_info.tags:
+            inferred_task_name = "inpainting"
         else:
-            class_name = model_info.config["diffusers"]["class_name"]
-            inferred_task_name = "stable-diffusion-xl" if "XL" in class_name else "stable-diffusion"
+            inferred_task_name = None
 
     elif library_name == "transformers":
         if model_info.pipeline_tag is not None:
@@ -165,7 +164,6 @@ def infer_task_from_model_name_or_path(model_name_or_path: str, revision: Option
                     if class_name_for_task == auto_model_class_name:
                         inferred_task_name = task_name
                         break
-
                     inferred_task_name = None
 
     else:
