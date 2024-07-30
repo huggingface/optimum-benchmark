04f713f0580386924544a8e61b4c6bb3d46c258f
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-07-30 17:33:40 +0200

    misc

--- optimum_benchmark/backends/llama_cpp/config.py
+++ optimum_benchmark/backends/llama_cpp/config.py
@@ -1,15 +1,8 @@
 from dataclasses import dataclass
-from logging import getLogger
 from typing import Optional
 
 from ...import_utils import llama_cpp_version
 from ..config import BackendConfig
-
-LOGGER = getLogger("backend")
-
-
-def llama_cpp_model_kwargs():
-    return {"verbose": True}
 
 
 @dataclass
@@ -19,16 +12,16 @@ class LlamaCppConfig(BackendConfig):
     _target_: str = "optimum_benchmark.backends.llama_cpp.backend.LlamaCppBackend"
 
     no_weights: bool = False
-    library: str = "llama_cpp"
     filename: Optional[str] = None
 
     def __post_init__(self):
+        self.library = "llama_cpp"
+        self.model_type = "llama_cpp"
+
         super().__post_init__()
 
-        self.device = self.device.lower()  # type: ignore
-        self.library = "llama_cpp"
+        if self.task not in ["feature-extraction", "text-generation"]:
+            raise NotImplementedError(f"Task {self.task} is not supported by LlamaCpp backend.")
 
-        if self.device not in ["cuda", "mps", "cpu"]:
-            raise ValueError(f"Llama.cpp Backend only supports 'cpu', 'mps' and 'cuda' devices, got {self.device}")
-
-        LOGGER.warning("Llama.cpp automatically selects the device, ignoring the device parameter in the config.")
+        if self.no_weights:
+            raise NotImplementedError("`no_weights` benchmarking is not supported by LlamaCpp backend.")
