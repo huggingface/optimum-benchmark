c551f03cd166c7359bb55a816521f7a2db5e999a
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-07-30 16:42:39 +0200

    misc

--- optimum_benchmark/backends/llama_cpp/config.py
+++ optimum_benchmark/backends/llama_cpp/config.py
@@ -12,14 +12,16 @@ class LlamaCppConfig(BackendConfig):
     _target_: str = "optimum_benchmark.backends.llama_cpp.backend.LlamaCppBackend"
 
     no_weights: bool = False
-    library: str = "llama_cpp"
     filename: Optional[str] = None
 
     def __post_init__(self):
+        self.library = "llama_cpp"
+        self.model_type = "llama_cpp"
+
         super().__post_init__()
 
-        self.device = self.device.lower()  # type: ignore
-        self.library = "llama_cpp"
+        if self.task not in ["feature-extraction", "text-generation"]:
+            raise NotImplementedError(f"Task {self.task} is not supported by LlamaCpp backend.")
 
-        if self.device not in ["cuda", "mps", "cpu"]:
-            raise ValueError(f"Llama.cpp Backend only supports 'cpu', 'mps' and 'cuda' devices, got {self.device}")
+        if self.no_weights:
+            raise NotImplementedError("`no_weights` benchmarking is not supported by LlamaCpp backend.")
