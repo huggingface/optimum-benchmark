e996b896372055ddb647ad34ea38c1140b11f9c0
baptiste <collebaptiste@gmail.com>
2024-08-08 12:15:18 +0000

    install from source with unpinned torch

--- setup.py
+++ setup.py
@@ -58,13 +58,14 @@ if USE_ROCM:
             "Please install amdsmi from https://github.com/ROCm/amdsmi to enable this feature."
         )
 
+
+
 if USE_ROCM:
     AUTOAWQ = "autoawq@https://github.com/casper-hansen/AutoAWQ/releases/download/v0.2.1/autoawq-0.2.1+rocm571-cp310-cp310-linux_x86_64.whl"
     AUTOGPTQ = "auto-gptq@https://huggingface.github.io/autogptq-index/whl/rocm573/auto-gptq/auto_gptq-0.7.1%2Brocm5.7.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
-else:
-    AUTOAWQ = "autoawq@git+https://github.com/casper-hansen/AutoAWQ.git"
-    AUTOAWQ_KERNELS = "autoawq-kernels@git+https://github.com/casper-hansen/AutoAWQ_kernels.git"
 
+else:
+    # AUTOAWQ will be installed from source via scripts/install_autoawq.py script
     AUTOGPTQ = "auto-gptq@git+https://github.com/PanQiWei/AutoGPTQ.git"
 
 EXTRAS_REQUIRE = {
@@ -81,7 +82,7 @@ EXTRAS_REQUIRE = {
     "py-txi": ["py-txi"],
     "vllm": ["vllm"],
     # optional dependencies
-    "autoawq": [AUTOAWQ_KERNELS, AUTOAWQ],
+    "autoawq": [],
     "auto-gptq": ["optimum", AUTOGPTQ],
     "sentence-transformers": ["sentence-transformers"],
     "bitsandbytes": ["bitsandbytes"],
@@ -92,6 +93,9 @@ EXTRAS_REQUIRE = {
     "timm": ["timm"],
     "peft": ["peft"],
 }
+
+if USE_ROCM:
+    EXTRAS_REQUIRE["autoawq"] = [AUTOAWQ]
 
 
 setup(
