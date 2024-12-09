24f4531e5d5c50b46bdcf8948cb3d6fa31c43e22
Ilyas Moutawwakil <57442720+IlyasMoutawwakil@users.noreply.github.com>
2024-12-09 16:50:33 +0100

    Remove non maintained backends (llm-swarm, inc) (#305)

--- optimum_benchmark/backends/neural_compressor/utils.py
+++
@@ -1,5 +0,0 @@
-from optimum.intel.neural_compressor.utils import _HEAD_TO_AUTOMODELS
-
-TASKS_TO_INCMODELS = {
-    task: f"optimum.intel.neural_compressor.{incmodel_name}" for task, incmodel_name in _HEAD_TO_AUTOMODELS.items()
-}
