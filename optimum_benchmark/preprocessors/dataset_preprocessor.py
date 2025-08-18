7c009f9ad31a7f4c02d49fe3d9ef15bd3752b5c6
Sasha <sasha.luccioni@huggingface.co>
2025-08-18 13:28:34 -0400

    oops

--- optimum_benchmark/preprocessors/dataset_preprocessor.py
+++ optimum_benchmark/preprocessors/dataset_preprocessor.py
@@ -292,14 +292,15 @@ def text_generation_preprocessing(
             padding=padding,
         )
     def reasoning_tokenize_function(examples):
-        chat_input= pretrained_processor.apply_chat_template(
+        return pretrained_processor.apply_chat_template(
             [{"role": "user", "content": examples[scenario_config.text_column_name]}],
             truncation=scenario_config.truncation,
             max_length=max_length - new_tokens,
             padding=padding,
             add_generation_prompt=True,
             enable_thinking=True,
-            return_dict=True,
+            tokenize= True,
+            return_dict=True)
 
     if scenario_config.reasoning == True:
         dataset = dataset.map(
